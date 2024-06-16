f473ef8db00a807576bc69f03e0aa190a07f991c
Rayan <ray.guet@yahoo.fr>
2024-06-16 17:23:13 +0200

    creation des bouton arrondi

--- RoundedIconButton.java
+++ RoundedIconButton.java
@@ -6,31 +6,31 @@ import javax.swing.JMenu;
 public class RoundedIconButton {
 
     public RoundedIconButton(ImageIcon homeIcon) {
-        // TODO Auto-generated constructor stub
+
     }
 
     public void setToolTipText(String string) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'setToolTipText'");
     }
 
     public void setBorderPainted(boolean b) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'setBorderPainted'");
     }
 
     public void setContentAreaFilled(boolean b) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'setContentAreaFilled'");
     }
 
     public void addActionListener(ActionListener actionListener) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'addActionListener'");
     }
 
     public void setFocusPainted(boolean b) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'setFocusPainted'");
     }
 
